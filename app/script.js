3ccbffff8760351fb004d1addc185f1644819d0a
Sanganu <kppkmsanu@gmail.com>
2020-05-07 23:39:46 -0600

    JSON Object stringify and table display and uuid package

--- app/script.js
+++ app/script.js
@@ -3,6 +3,26 @@ $.ajax({
     method: "GET"
 }).then(function (response) {
     console.log("response", response)
+    let mainObj = $("<table>");
+    let list=$("<tbody>")
+    for(let i=0;i<response.length;i++){
+        let row = $("<tr>")
+        let item1 = $("<td>")
+        item1.text(`${response[i].itemdescription}`);
+        row.attr("data-id",response[i].id);
+        row.append(item1);
+        let item2 = $("<td>")
+        item2.text(`${response[i].shopname}`);
+        row.append(item2);
+        let item3 = $("<td>")
+        item3.text(`${response[i].quantity}`);
+        let item4 = $("<td>")
+        item4.append(`<button class="deleteitem">Delete</button>`)
+        row.append(item4);
+        list.append(row)
+    }
+    mainObj.append(list)
+    $("#itemdisplay").append(mainObj)
 }).catch(function (error) {
     console.log("Error ", error)
 })
@@ -19,6 +39,7 @@ $("#additem").on("click", function () {
         data: itementry
     }).then(function (response) {
         console.log("Response", response)
+        // location.reload();
     }).catch(function (error) {
         console.log("Error -", error)
     })
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,14 +1,22 @@
-var router = require("express").Router()
-var db = require("../db/data.json") || [];
-var fs = require("fs");
+const router = require("express").Router()
+const fs = require("fs");
+let  db = require("../db/data.json") ;
+const { v1: uuidv1 } = require('uuid');
 router.get("/items",function(request,response){
+   
     response.json(db)
 });
 
 router.post("/item",function(request,response){
     console.log(request.body);
-    db.push(request.body);
-    fs.writeFile("db/data.json",db,function(err,data){
+    var record = {
+        id: uuidv1(),
+        itemdescription:request.body.itemdescription,
+        quantity: request.body.quantity,
+        shopname: request.body.shopname
+    }
+    db.push(record);
+    fs.writeFile("db/data.json",JSON.stringify(db),function(err,data){
         if(err){
             console.log("Unable to write data to file",err)
             response.json(err)
